0b15ba3e05ca60b28df0a3e678227535a6a0051e
Paulo Palmeira <paulopalmeirasp@gmail.com>
2023-05-13 11:29:40 -0300

    rodando

--- Unid2Cod3.py
+++ Unid2Cod3.py
@@ -3,10 +3,9 @@
 # read_csv para ler o dataset contido no arquivo USD_BRL_hist.csv.
 
 from pandas import read_csv
-from matplotlib import pyplot
+import matplotlib.pyplot as plt
 
-series = read_csv(r"<informe_sua_pasta>\USD_BRL_hist.csv", header=0,
-index_col=0, parse_dates=True, squeeze=True)
+df = read_csv("USD_BRL_hist.csv", header=0, index_col=0, parse_dates=True)
 
-series.plot()
-pyplot.show()
+plt.plot(df)
+plt.show()
